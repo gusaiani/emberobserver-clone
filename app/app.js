0cde8941d95471367f84f305c7a80da61af424c5
Gustavo Saiani <gustavo@poe.ma>
2016-09-14 13:19:37 -0300

    Add dotfiles, rcfiles and essential /app files

---
+++ app/app.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
+
+let App;
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+if (config.environment === 'production') {
+
+  Ember.onerror = function(error) {
+
+    if (window.trackJs) {
+      window.trackJs.track(error);
+    }
+
+    Ember.Logger.assert(false, error);
+
+    };
+
+  Ember.RSVP.on('error', function(error) {
+
+    if (window.trackJs) {
+      window.trackJs.track(error);
+    }
+
+    Ember.Logger.assert(false, error);
+
+  });
+}
+
+if (config.environment === 'development' || config.environment === 'test') {
+  Error.stackTraceLimit=200;
+}
+
+export default App;
---
+++ app/router.js
@@ -0,0 +1,72 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+  metrics: Ember.inject.service(),
+
+  didTransition() {
+    this._super(...arguments);
+    this._trackPage();
+  },
+  previousPage: null,
+  _trackPage() {
+    Ember.run.scheduleOnce('afterRender', this, () => {
+      let page = document.location.pathname;
+      let title = this.getWithDefault('currentRouteName', 'unknown');
+      let previousPage = this.get('previousPage');
+      let hasQuery = /query=/.test(document.location.search);
+
+      if (hasQuery) {
+        page = `${page}/?query=`;
+      }
+
+      if (page !== previousPage) {
+        this.set('previousPage', page);
+        Ember.get(this, 'metrics').trackPage({ page, title });
+      }
+    });
+  }
+});
+
+Router.map(function() {
+  this.route('categories', function() {
+    this.route('show', { path: '/:slug' });
+  });
+
+  this.route('addons', function() {
+    this.route('show', { path: '/:name' });
+    this.route('correct', { path: '/:name/correct' });
+    this.route('top', { path: '/lists/top' });
+  });
+
+  this.route('lists', function() {
+    this.route('top-addons');
+    this.route('new-addons');
+    this.route('recently-scored-addons');
+  });
+
+  this.route('maintainers', function() {
+    this.route('show', { path: '/:name' });
+  });
+
+  this.route('login');
+  this.route('admin', function() {
+    this.route('categories', function() {
+      this.route('new');
+      this.route('edit', { path: '/:slug' });
+    });
+    this.route('build-servers');
+    this.route('build-results', function() {
+      this.route('show', { path: '/:id' });
+    });
+  });
+
+  this.route('about');
+
+  this.route('model-not-found');
+  this.route('not-found', { path: '/*path' });
+});
+
+export default Router;
