83bb7e5b0582a9d5a1ce58fa36201d0adb8331da
Gustavo Saiani <gustavo@poe.ma>
2016-10-03 09:52:46 -0300

    Add jscs rules

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,7 +12,7 @@ module.exports = function(defaults) {
   //
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
-  // object’s keys should be the environment name and the values
+  // object's keys should be the environment name and the values
   // should be the asset to use in that environment.
   //
   // If the library that you are including contains AMD or ES6
