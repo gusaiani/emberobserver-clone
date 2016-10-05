7eb5ac570b899bccf7cc428440023211d2c312f6
Gustavo Saiani <gustavo@poe.ma>
2016-10-05 13:26:55 -0300

    Add test helpers

---
+++ tests/helpers/start-app.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import Application from '../../app';
+import config from '../../config/environment';
+import './qunit-assertions';
+import './mock-analytics';
+import LocalStore from '../../utils/local-storage';
+import mockStorage from './local-storage-mock';
+
+import './visit-addon';
+
+export default function startApp(attrs) {
+  let application;
+
+  LocalStore.storage = function() {
+    return mockStorage;
+  };
+
+  let attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+
+  Ember.run(() => {
+    application = Application.create(attributes);
+    application.setupForTesting();
+    application.injectTestHelpers();
+  });
+
+  return application;
+}
